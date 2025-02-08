4da6d327eb92fcc26898aa0e6bd27290ccbc8048
Mikhail Stepanov <26812468+mikestepanov@users.noreply.github.com>
2025-02-08 15:58:43 -0600

    test1

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+  console.log('1');
     const marginIssue = import.meta.env.VITE_MARGIN_ISSUE === 'true';
     console.log('VITE_MARGIN_ISSUE:', marginIssue);
     if (marginIssue) {
